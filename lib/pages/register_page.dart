1d6a6998d90f0ce4fb7398022c2a4fb3f9f31ba4
diegoalvis <diegoalvispal@gmail.com>
2019-03-17 00:12:36 -0300

    WIP Regsiter page step 1

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -13,34 +13,18 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget build(BuildContext context) {
     final strings = MyLocalizations.of(context);
 
-    Widget _buildPageOne = SafeArea(
-      child: Column(
-        children: [
-          Expanded(
-            child: Container(
-              alignment: Alignment.center,
-              child: Text(
-                'Como Eres?',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 45,
-                  fontFamily: 'BettyLavea',
-                ),
-              ),
-            ),
+    Widget _buildPageOne = Column(
+      children: [
+        Expanded(
+          child: Text(
+            "Selecciona las fotos...",
+            textAlign: TextAlign.center,
           ),
-          Expanded(
-            child: Text(
-              "Selecciona las fotos...",
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Expanded(
-            child: Container(alignment: Alignment.bottomCenter, child: Text("Fotos aqui")),
-          ),
-        ],
-      ),
+        ),
+        Expanded(
+          child: Container(alignment: Alignment.bottomCenter, child: Text("Fotos aqui")),
+        ),
+      ],
     );
 
     Widget _buildBottomButtons = new Row(
@@ -82,6 +66,32 @@ class _RegisterPageState extends State<RegisterPage> {
       ],
     );
 
-    return Scaffold(body: _buildBottomButtons);
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                alignment: Alignment.center,
+                child: Text(
+                  'Como Eres?',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 45,
+                    fontFamily: 'BettyLavea',
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 4,
+              child: _buildPageOne,
+            ),
+            _buildBottomButtons,
+          ],
+        ),
+      ),
+    );
   }
 }
