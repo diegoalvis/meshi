bd551a0fa628306b415a4e94b9889a0dcba67063
diegoalvis <diegoalvispal@gmail.com>
2019-05-24 10:30:19 -0300

    Pull to develop

--- lib/pages/interests_profile_page.dart
+++ lib/pages/interests_profile_page.dart
@@ -3,3 +3,12 @@
  * Copyright (c) 2019 - All rights reserved.
  */
 
+import 'package:flutter/material.dart';
+
+class InterestsProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
