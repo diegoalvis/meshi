ec7c6168e1e47f29a5c057652757cf76c45ea6d5
Waalvis <alvis.wap@gmail.com>
2019-06-17 17:50:16 -0500

    layout premium

--- lib/pages/home/premium/premium_page.dart
+++ lib/pages/home/premium/premium_page.dart
@@ -7,7 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:meshi/pages/home/home_section.dart';
 import 'package:meshi/utils/custom_widgets/list_premium.dart';
 
-
 class PremiumPage extends StatelessWidget with HomeSection {
   @override
   Widget build(BuildContext context) {
@@ -17,32 +16,59 @@ class PremiumPage extends StatelessWidget with HomeSection {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.fromLTRB(150, 40, 20, 20),
-              child: Text(
-                "Se premium",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 34,
-                  fontFamily: 'BettyLavea',
-                ),
+            Container(
+              decoration: BoxDecoration(
+
+                  gradient: LinearGradient(
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                      colors: [
+                    Color(0xff5E2531),
+                    Color(0xff80065E),
+                  ])),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(130, 40, 20, 20),
+                    child: Text(
+                      "Se premium",
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.onError,
+                        fontSize: 34,
+                        fontFamily: 'BettyLavea',
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(height: 25),
+                      Text('Chatea sin Limites',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onError,
+                          )),
+                      SizedBox(height: 16),
+                      Text('Participa por citas Regalo',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onError,
+                          )),
+                      SizedBox(height: 16),
+                      Text('Conoce mas personas de tu gusto',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onError,
+                          )),
+                      SizedBox(height: 16),
+                      Text('Mira en que coincides con tu pareja',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onError,
+                          )),
+                      SizedBox(height: 16),
+                    ],
+                  )
+                ],
               ),
             ),
-            ListTile(
-              title: Text('Chatea sin Limites'),
-            ),
-            ListTile(
-              title: Text('Participa por citas Regalo'),
-            ),
-            ListTile(
-              title: Text('Conoce mas personas de tu gusto'),
-            ),
-            ListTile(
-              title: Text('Mira en que coincides con tu pareja'),
-            ),
-
-            ListPremium()
+            Container(child: ListPremium())
           ],
         ),
       ),
