b36a3899f8e23148bdf9402c120a357b99533ac0
diegoalvis <diegoalvispal@gmail.com>
2020-03-10 12:00:34 +0100

    ios version 0.2.2 generated

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/diegoalvis/code/flutter/sdk"
+export "FLUTTER_ROOT=/Users/diegoalvis/code/Flutter/SDK"
 export "FLUTTER_APPLICATION_PATH=/Users/diegoalvis/code/flutter/meshi/meshi-app"
-export "FLUTTER_TARGET=/Users/diegoalvis/code/flutter/meshi/meshi-app/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/diegoalvis/code/flutter/sdk/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=0.1.8"
+export "FLUTTER_FRAMEWORK_DIR=/Users/diegoalvis/code/Flutter/SDK/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_BUILD_NAME=0.2.2"
 export "FLUTTER_BUILD_NUMBER=1"
-export "TRACK_WIDGET_CREATION=true"
